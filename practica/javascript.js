dda6ddef00b3b9dfaf90f769c612d2ebf15adde4
Aldemar8322 <162385518+Aldemar8322@users.noreply.github.com>
2024-04-16 19:54:02 -0500

    cambios

---
+++ practica/javascript.js
@@ -0,0 +1,55 @@
+"use strict";
+/* saludo1= 'hola'
+var saludo2= 'hola2'
+let saludo3= 'hola3'
+const saludo4= 'hola4'
+console.log(saludo1)
+console.log(saludo2)
+console.log(saludo3)
+console.log(saludo4) *//* 
+const numero1 = Number(prompt('ingrese el primer numero'));
+const numero2 = Number(prompt('ingrese el segundo numero'));
+console.log (numero1 + numero2);
+document.write (numero1 + numero2) */
+/* const numero = parseInt (prompt("Ingrese un numero)"))
+for (let i = 1; i <= numero; i++){
+console.log("ciclo: ", i)
+document.write (i)
+} *//* 
+let centinela = true
+while (centinela === true) {
+    console.log('ejecutando contenido del bloque');
+    centinela = false;
+} */
+/* let texto= prompt ('ingrese un texto')
+while (texto==="") {
+    texto = prompt ('ingrese un texto');
+}
+document.write ('ingresando el texto: ' + texto) */
+/* et miarreglo = ["hola",true,8,null];
+
+console.log(miarreglo);
+console.log(miarreglo[2]); */
+
+/* const numero = parseInt(prompt('Ingrese un numero'));
+console.log(numero)
+for (let i = 1; i <= numero; i++) {
+    if (i % 2 === 0)
+    document.write(i + " ");
+} */
+
+// Hacer un programa que solicite al usuario un numero de 0 a 6 y manejar errores y mostrar el dia de la semana correspondiente segun arreglo "dias"
+
+const arreglo = [
+    'domingo',
+    'lunes',
+    'martes',
+    'miércoles',
+    'jueves',
+    'viernes',
+    'sábado',
+  ];
+
+
+  // hacer un programa que solicite al usuario un dia de la semana y le retorne su nombre en ingles
+
